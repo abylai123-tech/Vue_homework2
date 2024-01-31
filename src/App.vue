6dabff2c2ac59d5c0a392215b67d643253c51984
Abeke <abylai.oryntaymail.ru>
2024-01-31 18:42:48 +0600

    Practice of Vue

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <Header title="My Awesome Store" />
+    <ProductCard
+      productName="Product 1"
+      description="This is a great product."
+      price="$19.99"
+    />
+    <Button buttonText="Buy Now" buttonColor="#27ae60" />
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import ProductCard from "./components/ProductCard.vue";
+import Button from "./components/Button.vue";
+
+export default {
+  components: {
+    Header,
+    ProductCard,
+    Button
+  }
+};
+</script>
+
+<style scoped>
+
+</style>  
